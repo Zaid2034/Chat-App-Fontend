dbf36a326b590ff5c574f72869a0f4b1b49cece5
zaid <mdzaidali2034@gmail.com>
2024-07-27 19:48:12 +0530

    Modify Base Url

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {UserContextProvider } from './UserContext'
 import Routes from './Routes'
 
 function App() {
-  axios.defaults.baseURL='http://localhost:4000'
+  axios.defaults.baseURL='https://chat-app-backend-fawn-psi.vercel.app/'
   axios.defaults.withCredentials=true
   return (
     <>
