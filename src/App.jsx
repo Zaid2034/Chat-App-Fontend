efc0ef9fbeddba80c6c52883965ad5c832968840
zaid <mdzaidali2034@gmail.com>
2024-07-27 19:28:15 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import './App.css'
+import axios from 'axios'
+import {UserContextProvider } from './UserContext'
+import Routes from './Routes'
+
+function App() {
+  axios.defaults.baseURL='http://localhost:4000'
+  axios.defaults.withCredentials=true
+  return (
+    <>
+      <UserContextProvider>
+        <Routes/>
+      </UserContextProvider>
+      
+     
+    </>
+  )
+}
+
+export default App
