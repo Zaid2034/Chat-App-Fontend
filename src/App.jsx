3449ffbe65a8cc08b0597ba237577d5f68cddf22
zaid <mdzaidali2034@gmail.com>
2024-07-27 19:50:17 +0530

    Resolve cors error

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Routes from './Routes'
 
 function App() {
   axios.defaults.baseURL='https://chat-app-backend-fawn-psi.vercel.app/'
-  axios.defaults.withCredentials=true
   return (
     <>
       <UserContextProvider>
